71e4663008ab19d9749c20e7f0907cd648e5e444
Ian Williams <ianwilliams633@gmail.com>
2015-08-12 12:56:43 -0700

    Missing semicolon and changed script type to text/html

--- addon/utils/utility-emblr.js
+++ addon/utils/utility-emblr.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export function tumblrParser(html) {
   var posts = [];
 
-  var domPosts = Ember.$(Ember.$(html).filter("#tumblr-posts").html()).filter(".post")
+  var domPosts = Ember.$(Ember.$(html).filter("#emblr-tumblr-posts").html()).filter(".post");
 
   function parse(obj, element) {
     Ember.$(element).children().each(function(index, attr) {
