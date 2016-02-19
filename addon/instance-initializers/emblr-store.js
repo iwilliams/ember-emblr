533deefb9d79dc779fd86b26473a9391d2c1f2f9
John-Henry Liberty <johnhenry.liberty@gmail.com>
2016-02-19 04:42:55 -0600

    Updates initializer and instanceinitializer to be compatible with > 2.3.0. Should be backwards compatible with < 2.3.0 as well

--- addon/instance-initializers/emblr-store.js
+++ addon/instance-initializers/emblr-store.js
@@ -1,11 +1,11 @@
 export function initialize(applicationInstance) {
-    // The docs say this should just be applicationInstance.lookup, but that doesn't seem to work :/
-    let config     = applicationInstance.container.lookupFactory('config:environment');
-    let emblrStore = applicationInstance.container.lookup('emblrStore:main');
-    emblrStore.set("queryBase",(config.environment === "production") ? "http://" + config.tumblr + ".tumblr.com/" : config.baseURL + "tumblr-mock/");
+  const config = applicationInstance.resolveRegistration('config:environment');
+  const emblrStore = applicationInstance.lookup('emblrStore:main');
+
+  emblrStore.set("queryBase", (config.environment === "production") ? `http://${config.tumblr}.tumblr.com/` : `${config.baseURL}tumblr-mock/`);
 }
 
 export default {
-    name: 'emblr-store',
-    initialize: initialize
+  name: 'emblr-store',
+  initialize: initialize
 };
