c7d92ca364e6f18165e51cbf88259705b4bc9f66
Ian Williams <ianwilliams633@gmail.com>
2016-04-01 17:54:44 -0700

    Update production url

--- addon/instance-initializers/emblr-store.js
+++ addon/instance-initializers/emblr-store.js
@@ -2,7 +2,8 @@ export function initialize(applicationInstance) {
     // The docs say this should just be applicationInstance.lookup, but that doesn't seem to work :/
     let config     = applicationInstance.container.lookupFactory('config:environment');
     let emblrStore = applicationInstance.container.lookup('emblrStore:main');
-    emblrStore.set("queryBase",(config.environment === "production") ? "http://" + config.tumblr + ".tumblr.com/" : config.baseURL + "tumblr-mock/");
+
+    emblrStore.set("queryBase",(config.environment === "production") ? "http://" + window.location.hostname "/" : config.baseURL + "tumblr-mock/");
 }
 
 export default {
